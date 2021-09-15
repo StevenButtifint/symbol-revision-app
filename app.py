a1ee7a5c0407a2cbdd5154944f0ca439765a6218
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-09-15 23:29:42 +0100

    added imports

---
+++ app.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+import random
+import glob
+import os
+
+from tkinter import mainloop, Tk, Frame, Grid, N, E, S, W, Button, StringVar, OptionMenu, Label
+from PIL import Image, ImageTk
+from os import listdir
