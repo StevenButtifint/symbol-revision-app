16de2133fae92e55fc5c514ec796bc2a98be73d6
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-09-15 23:30:43 +0100

    added class initialisation

--- app.py
+++ app.py
@@ -6,3 +6,23 @@ import os
 from tkinter import mainloop, Tk, Frame, Grid, N, E, S, W, Button, StringVar, OptionMenu, Label
 from PIL import Image, ImageTk
 from os import listdir
+
+
+
+
+class symbolRevisionWindow:
+    def __init__(self, parent):
+        self.window = parent
+        self.window.geometry("700x980")
+        self.window.title("Symbol Reviser")
+
+        self.packs = self.getPacks("res/packs")
+        self.current_pack = self.packs[0]
+
+        self.pack_items = []
+        self.introduced_items = []
+        self.lives = 3
+        self.full_grid = False
+        self.correct_item = ""
+
+
