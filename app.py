8a30cda8e1c50d67b24a72e2aac60a9e9aca11ca
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2021-09-21 14:00:38 +0100

    added makeHomePage class function

--- app.py
+++ app.py
@@ -26,3 +26,18 @@ class symbolRevisionWindow:
         self.correct_item = ""
 
 
+    def makeHomePage(self):
+        homeFrame = self.makeFrame(self.window, 0.5, 0.5, 1, 1, "center", "green")
+
+        question_label = Label(homeFrame, font=("Avalon", 15), text="Pick the symbol pack to learn:", bg="green")
+        question_label.place(relx=0.5, rely=0.4, anchor="center")
+        
+        pack_choice = StringVar(homeFrame)
+        pack_choice.set(self.current_pack) # default choice
+        w = OptionMenu(homeFrame, pack_choice, *self.packs)
+        w.place(relx=0.5, rely=0.5, relw=0.2, relh=0.04, anchor="center")
+        
+        start_btn = Button(homeFrame, font=("Avalon", 15), text="Start", command=lambda: self.startSession(pack_choice.get()))
+        start_btn.place(relx=0.5, rely=0.7, relw=0.2, relh=0.05, anchor="center")
+
+
